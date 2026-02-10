ae5b8569c5e4516ef3c8e6a4674a2289088ff1b2
Nelson-blur <nelsonogodo2@gmail.com>
2026-02-10 12:18:17 +0000

    5

--- Mock Exam Work/Mock Exam Work/obj/Debug/net9.0/Mock Exam Work.AssemblyInfo.cs
+++ Mock Exam Work/Mock Exam Work/obj/Debug/net9.0/Mock Exam Work.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mock Exam Work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27a92e10b750a0e1bebc73a466466dfccf29b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d669e97040ea81d5419566a4ecb0d806acce54be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mock Exam Work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mock Exam Work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
